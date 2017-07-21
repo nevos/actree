f535ae46f06b54027162bf48f4bf7bc96f5babfd
Nevos <nevonat@gmail.com>
2017-07-21 14:56:09 +0300

    actree initial

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("my node actree") ;
+
+actree = require('./actree');
+
+var o1 = { a: 1 };
+var o2 = { b: 2 };
+var o3 = { c: 3 };
+//var a1 = actree.addWordToTree(o1,o2) ;
+//console.log(actree.addWordToTree(a1,o3)) ;
+
